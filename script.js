7414c879a7395ba50051bf5e46fed40d6d747a9e
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-06-21 10:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	let index=0;
+  let res=" ";
+	while(s[index]!=' '){
+		res+=s[index];
+		index++;
+	}
+	return res;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
